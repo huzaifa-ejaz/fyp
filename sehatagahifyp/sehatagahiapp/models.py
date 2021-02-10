20c2c22eda0ccde5c4d0ff6ba9a8f558f92e1e98
huzaifa-ejaz <huzaifaejaz174@gmail.com>
2021-02-10 20:23:46 +0500

    Basic data models created

--- sehatagahifyp/sehatagahiapp/models.py
+++ sehatagahifyp/sehatagahiapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Therapist(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    mobile_no = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f"{self.id}: Name: {self.name} Email: {self.email}"
+
+class Track(models.Model):
+    name = models.CharField(max_length=64)
+    
+class Patient(models.Model):
+    name = models.CharField(max_length=64)
+    father_name = models.CharField(max_length=64)
+    gender = models.CharField(max_length=64)
+    dob = models.DateField()
+    mobile_no = models.CharField(max_length=11)
+    condition_type = models.CharField(max_length=64)
+    condition_tone = models.CharField(max_length=64)
+    therapist = models.ForeignKey(Therapist, on_delete=models.SET_NULL, null=True, related_name="patients")
+    tracks = models.ManyToManyField(Track, blank=True, related_name="patients")
+
+
+
+
+
+
+
+
+
