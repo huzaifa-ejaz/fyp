6590074d992b41bcee32570abfb4e0b22ad52d6b
huzaifa-ejaz <huzaifaejaz174@gmail.com>
2021-02-23 23:00:29 +0500

    Created therapist register feature (basic)

--- sehatagahifyp/sehatagahiapp/urls.py
+++ sehatagahifyp/sehatagahiapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("therapist-register", views.therapist_register, name="therapist-register")
 ]
 
 
--- sehatagahifyp/sehatagahiapp/views.py
+++ sehatagahifyp/sehatagahiapp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from .models import Therapist
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+
+
 
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world!")
+	users = get_user_model().objects.all()
+	therapists = Therapist.objects.all()
+	return render(request, "sehatagahiapp/index.html",{"my_users": users, "therapists": therapists})
+
+def therapist_register(request):
+	if request.method == 'POST':
+		user = get_user_model().objects.create_user(username = request.POST["username"] , password = request.POST["password"]  , is_therapist = True)
+		user.save()
+		t = Therapist(user = user, name = request.POST["name"], mobile_no = request.POST["mobilenumber"] )
+		t.save()
+	    
+		return HttpResponseRedirect(reverse("index"))
+	return render(request, "sehatagahiapp/register.html")
