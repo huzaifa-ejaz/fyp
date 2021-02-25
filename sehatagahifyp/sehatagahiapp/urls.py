f3b5e39b4b69848e7e5b3232739b2f4f79c21e52
huzaifa-ejaz <huzaifaejaz174@gmail.com>
2021-02-25 21:58:43 +0500

    Therapist-login created

--- sehatagahifyp/sehatagahiapp/urls.py
+++ sehatagahifyp/sehatagahiapp/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("therapist-register", views.therapist_register, name="therapist-register")
+    path("therapist-register", views.therapist_register, name="therapist-register"),
+    path("logout", views.logout_view, name="logout"),
+    path("therapist-login", views.login_view, name="therapist-login")
 ]
 
 
