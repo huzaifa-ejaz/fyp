3d33c4aea00b1cf2d5846d6b9356b40d738a726e
Omer Abid <49743149+OmerAbid98@users.noreply.github.com>
2021-06-11 00:12:18 +0500

    final final

--- sehatagahifyp/sehatagahiapp/forms.py
+++ sehatagahifyp/sehatagahiapp/forms.py
@@ -25,7 +25,7 @@ class Therapist_Register_form(forms.Form):
     Name = forms.CharField(label='Full Name',max_length=25, validators=[validator.RegexValidator(regex=r'^[a-zA-Z][a-zA-Z ]+$')])
     Username=forms.CharField(label='User Name',max_length=25,validators=[username_val])
     MobileNumber = forms.CharField(label='Mobile Number 03xxxxxxxxx',max_length=11,validators=[validator.RegexValidator(regex=r'^[0][3][0-9]+$'), validator.MinLengthValidator(11)])
-    WorkEmail = forms.EmailField(label='Work Email')
+    WorkEmail = forms.CharField(label='Work Email',validators=[validator.RegexValidator(regex=r'^.+[@sehatagahi.com]$',message='The email format is wrong.')])
     Password = forms.CharField(label='Password',max_length=10, widget=forms.PasswordInput,validators=[validator.MinLengthValidator(limit_value=6)])
     PasswordRe= forms.CharField(label='Re-enter Password',max_length=10, widget=forms.PasswordInput,validators=[validator.MinLengthValidator(limit_value=6)])
     SecurityQs1 = forms.CharField(label='Security Qs1: First and Last letter of Father\'s name',max_length=2)
@@ -37,21 +37,20 @@ class Therapist_Register_form(forms.Form):
         email=cleaned_data.get("WorkEmail")
         if password!=passwordre:
             self.add_error('PasswordRe', 'The password does not match')
-        if '@sehatagahi.com' not  in email:
-            self.add_error('WorkEmail', 'Please enter the correct email')
-        
-        #Checking if the provided email is an authorized email
-        try:
-            authorizedEmail = AuthorizedEmail.objects.get(WorkEmail__exact=email)
-        except AuthorizedEmail.DoesNotExist:
-            self.add_error('WorkEmail','Please enter a valid email')
-        
-        #Checking if the provided email is already in use by another user
-        try:
-            therapist = Therapist.objects.get(WorkEmail__exact = email)
-            self.add_error('WorkEmail','This email is already in use by another user')
-        except Therapist.DoesNotExist:
-            pass
+
+        if email is not None:
+            #Checking if the provided email is an authorized email
+            try:
+                authorizedEmail = AuthorizedEmail.objects.get(WorkEmail__exact=email)
+            except AuthorizedEmail.DoesNotExist:
+                self.add_error('WorkEmail','Please enter a valid email')
+
+            #Checking if the provided email is already in use by another user
+            try:
+                therapist = Therapist.objects.get(WorkEmail__exact = email)
+                self.add_error('WorkEmail','This email is already in use by another user')
+            except Therapist.DoesNotExist:
+                pass
 
 
 
